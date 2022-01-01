0987e8f482ff78b458b03575f24a1a1fba4c3930
Grant Walker <GrantW2505@gmail.com>
2022-01-01 20:20:11 +0200

    Minor improvements

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ from PIL import Image
 from get_exif_data import get_exif_item, EXIF_DATETIME_FORMAT
 
 
-inputPath = 'raw-photos'
-outputPath = 'sorted-photos'
+input_path = 'raw'
+output_path = 'sorted'
 unsorted_dir_name = "_unsorted"
-
-unsorted_dir_path = outputPath + "/" + unsorted_dir_name
 
 SHORT_DATE_FORMAT = "%Y-%m-%d"
 
@@ -33,18 +31,17 @@ def processFile(dirEntry):
 
     try:
         with Image.open(dirEntry.path) as image:
-            copy = image.copy()
-            exif_DateTime = get_exif_item(image, "DateTime")  # exif ID 306
-
             # default directory is unsorted
-            dir = unsorted_dir_path
+            dir = output_path + "/" + unsorted_dir_name
+
+            exif_DateTime = get_exif_item(image, "DateTime")  # exif ID 306
 
             # reset dir name if exif date exists
             if exif_DateTime:
                 date = datetime.strptime(
                     exif_DateTime["value"], EXIF_DATETIME_FORMAT)
 
-                dir = outputPath + "/" + date.strftime(SHORT_DATE_FORMAT)
+                dir = output_path + "/" + date.strftime(SHORT_DATE_FORMAT)
 
             copyImage(image, dir, dirEntry.name)
 
@@ -61,10 +58,10 @@ def create_dir(path):
 
 
 def main():
-    print(f"Read from {inputPath}")
-    print(f"Copy To {outputPath}")
+    print(f"Read from {input_path}")
+    print(f"Copy To {output_path}")
 
-    processDir(inputPath)
+    processDir(input_path)
 
 
 if __name__ == "__main__":
