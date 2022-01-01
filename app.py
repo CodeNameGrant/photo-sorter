e6d0e2b729c36ed7afa9c58ef0c0efce5604473a
Grant Walker <GrantW2505@gmail.com>
2022-01-01 19:47:35 +0200

    Optimized code to be DRYer

--- app.py
+++ app.py
@@ -1,99 +1,70 @@
 import os
 from datetime import datetime
 from PIL import Image
-from PIL.ExifTags import TAGS
+
+from get_exif_data import get_exif_item, EXIF_DATETIME_FORMAT
+
 
 inputPath = 'raw-photos'
 outputPath = 'sorted-photos'
-outputPath_unsorted = outputPath + '/_unsorted'
+unsorted_dir_name = "_unsorted"
 
-dateTime_format = "%Y:%m:%d %H:%M:%S"
+unsorted_dir_path = outputPath + "/" + unsorted_dir_name
 
-tag_DateTime = 306
-tag_DateTimeOriginal = 36867
-tag_DateTimeDigitized = 36868
-tag_PreviewDateTime = 50971
+SHORT_DATE_FORMAT = "%Y-%m-%d"
 
 
 def processDir(dirPath):
     for dirEntry in os.scandir(dirPath):
-        processDirEntry(dirEntry)
+        if dirEntry.is_dir():
+            processDir(dirEntry.path)
+
+        else:
+            processFile(dirEntry)
 
 
-def processDirEntry(dirEntry):
-    if dirEntry.is_dir():
-        processDir(dirEntry.path)
-
-    else:
-        processFile(dirEntry)
-
-
-def getExifDate(image):
-    exifData = image.getexif()
-
-    if exifData is None or len(exifData) == 0:
-        # print('Sorry, image has no exif data.')
-        return None
-
-    for key, val in exifData.items():
-        if key in [tag_DateTime]:
-            return val
+def copyImage(image, dir, fileName):
+    create_dir(dir)
+    copy = image.copy()
+    copy.save(dir + "/" + fileName)
 
 
 def processFile(dirEntry):
-    print("============================")
-    print(f"processFile {dirEntry.path}")
 
     try:
         with Image.open(dirEntry.path) as image:
             copy = image.copy()
-            exifDate = getExifDate(image)
+            exif_DateTime = get_exif_item(image, "DateTime")  # exif ID 306
 
-            if exifDate:
-                date = datetime.strptime(exifDate, dateTime_format)
+            # default directory is unsorted
+            dir = unsorted_dir_path
 
-                try:
-                    path = outputPath + \
-                        "/" + date.strftime("%Y-%m-%d")
-                    os.mkdir(path)
-                    copy.save(path + "/" + dirEntry.name)
+            # reset dir name if exif date exists
+            if exif_DateTime:
+                date = datetime.strptime(
+                    exif_DateTime["value"], EXIF_DATETIME_FORMAT)
 
-                except FileExistsError:
-                    pass
+                dir = outputPath + "/" + date.strftime(SHORT_DATE_FORMAT)
 
-                print(exifDate, date)
-
-            else:
-                # pass
-                # unsorted
-                copy.save(outputPath_unsorted + "/" + dirEntry.name)
+            copyImage(image, dir, dirEntry.name)
 
     except OSError:
         print("Not an Image File")
         pass
 
-    # info = dirEntry.stat()
-    # print(f"processFile {dirEntry.path} {info}")
-    # print(datetime.utcfromtimestamp(info.st_atime))
-    # print(datetime.utcfromtimestamp(info.st_mtime))
-    # print(datetime.utcfromtimestamp(info.st_ctime))
+
+def create_dir(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
 
 
 def main():
     print(f"Read from {inputPath}")
     print(f"Copy To {outputPath}")
 
-    # Prepare outputDir
-    try:
-        os.mkdir(outputPath_unsorted)
-    except FileExistsError:
-        pass
-
     processDir(inputPath)
-
-    # for tag in TAGS:
-    #     if 'ext' in TAGS[tag].lower():
-    #         print(tag, TAGS[tag])
 
 
 if __name__ == "__main__":
